c00ff0c65400298d179fe7348eb6cb6d6e90f05d
Christopher Lamm <chris@theantichris.com>
2017-01-08 08:31:30 -0500

    Clean up.

--- app/Providers/SparkServiceProvider.php
+++ app/Providers/SparkServiceProvider.php
@@ -7,18 +7,8 @@ use AMP\User;
 use Laravel\Spark\Providers\AppServiceProvider as ServiceProvider;
 use Laravel\Spark\Spark;
 
-/**
- * Class SparkServiceProvider
- *
- * @package AMP\Providers
- */
 class SparkServiceProvider extends ServiceProvider
 {
-    /**
-     * Your application and company details.
-     *
-     * @var array
-     */
     protected $details = [
         'vendor'   => 'Additerra, Inc.',
         'product'  => 'Additerra Manufacturing Platform',
@@ -27,36 +17,16 @@ class SparkServiceProvider extends ServiceProvider
         'phone'    => '(865) 321-9199',
     ];
 
-    /**
-     * The address where customer support e-mails should be sent.
-     *
-     * @var string
-     */
     protected $sendSupportEmailsTo = 'support@additerra.com';
 
-    /**
-     * All of the application developer e-mail addresses.
-     *
-     * @var array
-     */
     protected $developers = [
         'chrislamm@additerra.com',
         'mattserkland@additerra.com',
         'willsames@additerra.com',
     ];
 
-    /**
-     * Indicates if the application will expose an API.
-     *
-     * @var bool
-     */
     protected $usesApi = true;
 
-    /**
-     * Finish configuring Spark for the application.
-     *
-     * @return void
-     */
     public function booted()
     {
         Spark::useBraintree()->noCardUpFront()->teamTrialDays(10);
