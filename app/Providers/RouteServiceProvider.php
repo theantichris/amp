5a8d13d92a13a353af0cb8195b2de665c25696e2
Christopher Lamm <chris@theantichris.com>
2017-01-08 10:29:10 -0500

    Moved API route to API file.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,77 +2,43 @@
 
 namespace AMP\Providers;
 
-use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Routing\Router;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
     protected $namespace = 'AMP\Http\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
-
         parent::boot();
     }
 
-    /**
-     * Define the routes for the application.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
     public function map(Router $router)
     {
         $this->mapWebRoutes($router);
-
         $this->mapApiRoutes($router);
-
-        //
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
     protected function mapWebRoutes(Router $router)
     {
         $router->group([
-            'namespace' => $this->namespace, 'middleware' => ['web', 'hasTeam'],
-        ], function ($router) {
+            'namespace'  => $this->namespace,
+            'middleware' => ['web', 'hasTeam'],
+        ], function () {
+            /** @noinspection PhpIncludeInspection */
             require base_path('routes/web.php');
         });
     }
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
     protected function mapApiRoutes(Router $router)
     {
         $router->group([
-            'namespace' => $this->namespace,
+            'namespace'  => $this->namespace,
             'middleware' => 'api',
-            'prefix' => 'api',
-        ], function ($router) {
+            'prefix'     => 'api',
+        ], function () {
+            /** @noinspection PhpIncludeInspection */
             require base_path('routes/api.php');
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,3 @@ Route::get('/', 'WelcomeController@show');
 Route::get('/home', 'HomeController@show');
 
 Route::get('/customer', 'Customer\CustomerController@index');
-
-Route::get('/api/customer', 'Customer\CustomerApiController@index');
