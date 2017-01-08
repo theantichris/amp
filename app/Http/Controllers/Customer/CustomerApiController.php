e80b79722b3530c75f53bd39236bca43621e4430
Christopher Lamm <chris@theantichris.com>
2017-01-08 10:19:34 -0500

    Injecting auth service into controller.

--- app/Http/Controllers/Customer/CustomerApiController.php
+++ app/Http/Controllers/Customer/CustomerApiController.php
@@ -6,16 +6,19 @@ use AMP\Http\Controllers\Controller;
 use AMP\Repository\CustomerRepository;
 use Illuminate\Http\JsonResponse;
 use Response;
+use Illuminate\Contracts\Auth\Factory as Auth;
 
 class CustomerApiController extends Controller
 {
     private $repo;
+    private $auth;
 
-    public function __construct(CustomerRepository $repo)
+    public function __construct(CustomerRepository $repo, Auth $auth)
     {
         $this->middleware('auth');
 
         $this->repo = $repo;
+        $this->auth = $auth->guard();
     }
 
     public function index(): JsonResponse
