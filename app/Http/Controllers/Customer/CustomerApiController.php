cd24ac899d8af9eabbb9a8751778d469ab98a90f
Christopher Lamm <chris@theantichris.com>
2017-01-15 09:53:45 -0500

    Using contextual injections with interface.

--- app/Http/Controllers/Customer/CustomerApiController.php
+++ app/Http/Controllers/Customer/CustomerApiController.php
@@ -3,7 +3,7 @@
 namespace AMP\Http\Controllers\Customer;
 
 use AMP\Http\Controllers\Controller;
-use AMP\Repository\Customer\CustomerRepository;
+use AMP\Repository\RepositoryInterface;
 use Illuminate\Contracts\Auth\Factory as Auth;
 use Illuminate\Http\JsonResponse;
 use Response;
@@ -13,11 +13,11 @@ class CustomerApiController extends Controller
     private $repo;
     private $auth;
 
-    public function __construct(CustomerRepository $repo, Auth $auth)
+    public function __construct(RepositoryInterface $customerRepo, Auth $auth)
     {
         $this->middleware('auth');
 
-        $this->repo = $repo;
+        $this->repo = $customerRepo;
         $this->auth = $auth->guard();
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,30 +2,27 @@
 
 namespace AMP\Providers;
 
+use AMP\Http\Controllers\Customer\CustomerApiController;
+use AMP\Repository\Customer\CustomerRepository;
+use AMP\Repository\RepositoryInterface;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         if ($this->app->environment() == 'local') {
             $this->app->register(IdeHelperServiceProvider::class);
         }
+
+        $this->app->when(CustomerApiController::class)
+                  ->needs(RepositoryInterface::class)
+                  ->give(CustomerRepository::class);
     }
 }
