2385a7c983080626a69e0a0a444127fa55ab81ad
Christopher Lamm <chris@theantichris.com>
2017-01-22 11:20:57 -0500

    Refactored customer seeding.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -5,9 +5,19 @@ $factory->define(AMP\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(\AMP\Domain\Customer\Customer::class, function (Faker\Generator $faker) {
+    return [
+        'account_number' => $faker->bankAccountNumber,
+        'company_name'   => $faker->name,
+        'contact_name'   => $faker->name,
+        'contact_email'  => $faker->unique()->safeEmail,
     ];
 });
--- database/seeds/CustomersSeeder.php
+++ database/seeds/CustomersSeeder.php
@@ -2,7 +2,6 @@
 
 use AMP\Domain\Customer\Customer;
 use AMP\Team;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CustomersSeeder extends Seeder
@@ -10,24 +9,12 @@ class CustomersSeeder extends Seeder
     public function run()
     {
         /** @var Team $additerra */
+        /** @noinspection PhpUndefinedMethodInspection */
         $additerra = Team::where('name', '=', 'Additerra, Inc.')->first();
 
-        Customer::create([
-            'account_number' => '123456789',
-            'company_name'   => 'Customer One',
-            'contact_name'   => 'John Doe',
-            'contact_email'  => 'john@customerone.com',
-            'team_id'        => $additerra->getQueueableId(),
-            'created_at'     => new Carbon(),
-        ]);
+        /** @var Customer $customer */
+        factory(Customer::class, 10)->create(['team_id' => $additerra->getQueueableId()])->each(function ($customer) {
 
-        Customer::create([
-            'account_number' => '987654321',
-            'company_name'   => 'Customer Two',
-            'contact_name'   => 'Jane Doe',
-            'contact_email'  => 'jane@customertwo.com',
-            'team_id'        => $additerra->getQueueableId(),
-            'created_at'     => new Carbon(),
-        ]);
+        });
     }
 }
