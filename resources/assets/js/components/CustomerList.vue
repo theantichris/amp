3e669e48e758b9d6b571311903f44956be6a6ace
Christopher Lamm <chris@theantichris.com>
2017-01-22 13:04:57 -0500

    Sorting list.

--- resources/assets/js/components/CustomerList.vue
+++ resources/assets/js/components/CustomerList.vue
@@ -1,15 +1,15 @@
-<template>
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
     <table class="table table-striped">
         <thead>
         <tr>
-            <th v-for="column in columns">
+            <th v-for="column in columns" v-on:click="sortBy(column)">
                 {{ column.replace( /([A-Z])/g, " $1" ) | capitalize }}
             </th>
         </tr>
         </thead>
 
         <tbody>
-        <tr v-for="customer in customers">
+        <tr v-for="customer in sortedCustomers">
             <td>{{ customer.accountNumber }}</td>
             <td>{{ customer.companyName }}</td>
             <td>{{ customer.contactName }}</td>
@@ -26,12 +26,14 @@
         data (){
             return {
                 customers: [],
+                sortedCustomers: [],
                 columns: [
                     'accountNumber',
                     'companyName',
                     'contactName',
                     'contactEmail',
-                ]
+                ],
+                sortKey: 'accountNumber'
             }
         },
 
@@ -45,9 +47,22 @@
 
                 resource.get().then((response) => {
                     this.customers = response.data;
+                    this.sortBy(this.sortKey);
                 }, (error) => {
                     console.error(error);
                 })
+            },
+            sortBy(column){
+                this.sortedCustomers = this.customers;
+
+                if (this.sortKey == column)
+                    this.sortedCustomers.reverse();
+                else
+                    this.sortedCustomers = this.customers.sort(function (a, b) {
+                        return a[column] == b[column] ? 0 : +(a[column] > b[column] || -1);
+                    });
+
+                this.sortKey = column;
             }
         }
     }
