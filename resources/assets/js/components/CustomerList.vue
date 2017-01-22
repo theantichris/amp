4364b36a0f3442978fb9dc712496e25f7e878021
Christopher Lamm <chris@theantichris.com>
2017-01-22 10:23:54 -0500

    Working on customer table.

--- resources/assets/js/components/CustomerList.vue
+++ resources/assets/js/components/CustomerList.vue
@@ -1,9 +1,19 @@
 <template>
-    <ul>
-        <li v-for="customers in customers">
-            {{ customers.company_name }}
-        </li>
-    </ul>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Account Number</th>
+            <th>Company Name</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr v-for="customer in customers">
+            <td>{{ customer.account_number }}</td>
+            <td>{{ customer.company_name }}</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
