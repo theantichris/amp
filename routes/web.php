34ee96e61a03f596202fd80d69f2a0608ece07af
Christopher Lamm <chris@theantichris.com>
2017-02-26 07:25:30 -0500

    Created route group for /customers.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ Route::get('/', 'WelcomeController@show');
 
 Route::get('/home', 'HomeController@show');
 
-Route::get('/customers', 'Customer\CustomerController@index');
+Route::group(['prefix' => 'customers'], function () {
+    Route::get('', 'Customer\CustomerController@index');
+});
