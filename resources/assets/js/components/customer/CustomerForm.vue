18307ce49afb71caf2797d9d202d7456176ddc37
Christopher Lamm <chris@theantichris.com>
2017-02-26 09:42:15 -0500

    Better preventing.

--- resources/assets/js/components/customer/CustomerForm.vue
+++ resources/assets/js/components/customer/CustomerForm.vue
@@ -1,5 +1,5 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml">
-    <form v-on:submit="save($event)">
+    <form v-on:submit.prevent="save">
         <div class="row">
             <div class="form-group col-xs-12 required" v-bind:class="{'has-error': form.errors.has('accountNumber')}">
                 <label for="accountNumber" class="control-label">Account Number</label>
@@ -76,10 +76,7 @@
         },
 
         methods: {
-            save(event) {
-                if (event)
-                    event.preventDefault();
-
+            save() {
                 Spark.post('/customers', this.form).then(response => {
                     console.log(response);
                 });
