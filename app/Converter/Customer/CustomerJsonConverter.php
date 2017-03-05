6c20ead16e61e0f458463775ff0b3afb195b436b
Christopher Lamm <chris@theantichris.com>
2017-03-05 09:54:20 -0500

    Optional fields.

--- app/Converter/Customer/CustomerJsonConverter.php
+++ app/Converter/Customer/CustomerJsonConverter.php
@@ -20,6 +20,38 @@ class CustomerJsonConverter implements JsonConverterInterface
                  ->setContactEmail($data->contactEmail)
                  ->team()->associate($team);
 
+        if (isset($data->contactPhone)) {
+            $customer->setContactPhone($data->contactPhone);
+        }
+
+        if (isset($data->address1)) {
+            $customer->setAddress1($data->address1);
+        }
+
+        if (isset($data->address2)) {
+            $customer->setAddress2($data->address2);
+        }
+
+        if (isset($data->city)) {
+            $customer->setCity($data->city);
+        }
+
+        if (isset($data->state)) {
+            $customer->setState($data->state);
+        }
+
+        if (isset($data->zip)) {
+            $customer->setZip($data->zip);
+        }
+
+        if (isset($data->shippingAccountProvider)) {
+            $customer->setShippingAccountProvider($data->shippingAccountProvider);
+        }
+
+        if (isset($data->shippingAccountNumber)) {
+            $customer->setShippingAccountNumber($data->shippingAccountNumber);
+        }
+
         return $customer;
     }
 }
--- resources/assets/js/components/customer/CustomerForm.vue
+++ resources/assets/js/components/customer/CustomerForm.vue
@@ -30,6 +30,56 @@
         </div>
 
         <div class="row">
+            <div class="form-group col-xs-12">
+                <label for="contactPhone" class="control-label">Contact Phone</label>
+                <input id="contactPhone" name="contactPhone" type="tel" class="form-control" v-model="form.contactPhone">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-xs-12">
+                <label for="address1" class="control-label">Address</label>
+                <input id="address1" name="address1" type="text" class="form-control" v-model="form.address1">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-xs-12">
+                <label for="address2" class="control-label">Address Line 2</label>
+                <input id="address2" name="address2" type="text" class="form-control" v-model="form.address2">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-xs-12 col-xs-4">
+                <label for="city" class="control-label">City</label>
+                <input id="city" name="city" type="text" class="form-control" v-model="form.city">
+            </div>
+
+            <div class="form-group col-xs-12 col-xs-4">
+                <label for="state" class="control-label">State</label>
+                <input id="state" name="state" type="text" class="form-control" v-model="form.state">
+            </div>
+
+            <div class="form-group col-xs-12 col-xs-4">
+                <label for="zip" class="control-label">Zip</label>
+                <input id="zip" name="zip" type="text" class="form-control" v-model="form.zip">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-xs-12 col-md-6">
+                <label for="shippingAccountProvider" class="control-label">Shipping Account Provider</label>
+                <input id="shippingAccountProvider" name="shippingAccountProvider" type="text" class="form-control" v-model="form.shippingAccountProvider">
+            </div>
+
+            <div class="form-group col-xs-12 col-md-6">
+                <label for="shippingAccountNumber" class="control-label">Shipping Account Number</label>
+                <input id="shippingAccountNumber" name="shippingAccountNumber" type="text" class="form-control" v-model="form.shippingAccountNumber">
+            </div>
+        </div>
+
+        <div class="row">
             <div class="col-xs-12">
                 <a href="/customers" class="btn btn-danger" title="Cancel">Cancel</a>
                 <button type="submit" class="btn btn-primary" v-bind:disabled="form.busy" title="Save">Save</button>
@@ -49,7 +99,15 @@
                     accountNumber: '',
                     companyName: '',
                     contactName: '',
-                    contactEmail: ''
+                    contactEmail: '',
+                    contactPhone: '',
+                    address1: '',
+                    address2: '',
+                    city: '',
+                    state: '',
+                    zip: '',
+                    shippingAccountProvider: '',
+                    shippingAccountNumber: ''
                 })
             }
         },
