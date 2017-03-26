cf2f0838f90736fdb74398e4e6ca134dc3981732
Christopher Lamm <chris@theantichris.com>
2017-03-26 11:29:58 -0400

    Added todo.

--- app/Repository/Repository.php
+++ app/Repository/Repository.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Repository
+ *
+ * @package AMP\Repository
+ *
+ * @TODO    : Clean up.
+ */
 abstract class Repository implements RepositoryInterface
 {
     /** @var  Builder */
