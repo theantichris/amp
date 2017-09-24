4c1b4a75d3669017b5dc92c44b72a9c30b2c4465
Christopher Lamm <chris@theantichris.com>
2017-09-24 11:02:48 -0400

    Rolled back some changes.

--- database/migrations/2016_11_20_143108_create_users_table.php
+++ database/migrations/2016_11_20_143108_create_users_table.php
@@ -1,15 +1,10 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
@@ -38,11 +33,6 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::drop('users');
