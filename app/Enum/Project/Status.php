a3281030e29f27cc97c3d22246aa7431dbc508e1
Christopher Lamm <chris@theantichris.com>
2017-10-14 17:21:34 -0400

    Statuses, updated Composer dependencies.

--- app/Enum/Project/Status.php
+++ app/Enum/Project/Status.php
@@ -6,5 +6,20 @@ use theantichris\enum\Enum;
 
 class Status extends Enum
 {
-    const NEW_PROJECT = 'New';
+    const NEW_PROJECT     = 'New';
+    const READY_TO_QUOTE  = 'Ready To Quote';
+    const QUOTE_GENERATED = 'Quote Generated';
+    const QUOTE_SENT      = 'Quote Sent';
+    const QUOTE_ACCEPTED  = 'Quote Accepted';
+    const QUOTE_REJECTED  = 'Quote Rejected';
+    const PRE_PRODUCTION  = 'Pre-Production';
+    const PRODUCTION      = 'Production';
+    const POST_PRODUCTION = 'Post-Production';
+    const QUALITY_CONTROL = 'Quality Control';
+    const SHIPPING        = 'Shipping';
+    const SHIPPED         = 'Shipped';
+    const DELIVERED       = 'Delivered';
+    const INVOICED        = 'Invoiced';
+    const PAID            = 'Paid';
+    const COMPLETE        = 'Complete';
 }
