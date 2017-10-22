ddf90fd0371c9cc6e21e105e87cd793d949fd1ff
Christopher Lamm <chris@theantichris.com>
2017-10-22 09:27:19 -0400

    Moved Materials.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,7 @@ require('./components/bootstrap');
 
 import CustomerManagement from "./components/customer/CustomerManagement.vue";
 import MachineProfileManagement from "./components/machine-profile/MachineProfileManagement.vue";
-import MaterialManagement from "./components/project/MaterialManagement.vue";
+import MaterialManagement from "./components/project/material/MaterialManagement.vue";
 
 new Vue({
     mixins: [require('spark')],
